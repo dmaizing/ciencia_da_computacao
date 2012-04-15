dd288db848be049cf46a0c59f62b12f68761d21b
Rafael etc <rafael.etc@gmail.com>
2012-04-14 21:50:26 -0300

    reorganizando

---
+++ lista2_pc/l02_02.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+#include<stdlib.h>
+
+/*
+Escreva um programa que leia um vetor A com 15 elementos numéricos inteiros e
+apresente o total de elementos pares existentes em A.
+*/
+
+int main() {
+	int i,a[15],cont;
+	cont=0;
+	for(i=0;i<=14;i++){
+		printf("Digite o valor de A[%d]=",i+1);
+		scanf("%d",&a[i]);
+		if (a[i]%2==0) 
+		cont=cont+1;
+	}
+	printf("Elementos pares em A=%d\n",cont);
+	return 0;
+}
---
+++ lista2_pc/l02_04.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+O produto escalar de dois vetores (matemáticos), v=[v1,v2,v3,...,vn] e
+u=[u1,u2,u3,...,un], é dado por v*u=v1*u1+v2*u2+v3*u3+...+un*vn .
+Escreva um programa que leia dois vetores A e B de dez elementos cada e
+apresente como resultado o cálculo do produto escalar entre A e B.
+*/
+
+int main(){
+	int i,a[10],b[10],c[10];
+	sc=0;
+	for(i=0;i<=9;i++){
+		printf("Digite o valor de A[%d]=",i+1);
+		scanf("%d",&a[i]);
+		printf("Digite o valor de B[%d]=",i+1);
+		scanf("%d",&b[i]);
+		c[i]=a[i]*b[i];
+		sc=sc+c[i];
+	}
+	printf("O produto escalar de AB=%d\n",sc);
+}
