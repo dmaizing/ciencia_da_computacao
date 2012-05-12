bc493ef9e433baf1040f57dc5ff8aa0bf6241977
Rafael etc <rafael.etc@gmail.com>
2012-05-12 14:56:03 -0300

    atualizando a exercicio 6 da lista 4

--- lista4_ppc/l04_06.c
+++ lista4_ppc/l04_06.c
@@ -8,11 +8,15 @@ int main ()
 	char copia1[13],copia2[13];
     int i,c=0;
     c=strlen(frase);
-    strncpy(copia1,frase,4);
+    //strncpy(copia1,frase,c-4);
+    //copia1[5]='\0';
     for(i=3;i<=c;i++) {
 		copia2[i]=frase[i];
-    }
-    puts(copia1);
-    puts(copia2);
+	}
+	copia2[13]='\0';
+	//puts(copia1);
+    //puts(copia2);
+    //printf("%s\n",copia1);
+    printf("%s\n",copia2);
     return 0;
 }
