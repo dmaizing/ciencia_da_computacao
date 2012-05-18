2a9b6c1147addb0e586b2682c9a1bf28c5afc872
Rafael etc <rafael.etc@gmail.com>
2012-05-17 22:32:40 -0300

    melhorando exercicio 4 da lista 4

--- lista4_ppc/l04_05.c
+++ lista4_ppc/l04_05.c
@@ -1,21 +1,21 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
- 
+
 int main ()
 {
-    char palavra[18],copia1[18],copia2[18];
-    int i,c=0;
-    printf("Entre com uma palavra: ");
-    gets(palavra);
-    c=strlen(palavra);
-    strcpy(copia1,palavra);
-    for(i=0;i<=c;i++) {
+	char palavra[18],copia1[18],copia2[18];
+	int i,c=0;
+	printf("Entre com uma palavra: ");
+	gets(palavra);
+	c=strlen(palavra);
+	strcpy(copia1,palavra);
+	for(i=0;i<=c;i++) {
 		copia2[i]=palavra[i];
-    }
-    printf("String copiada com strcpy:\n");
-    puts(copia1);
-    printf("String copiada com for:\n");
-    puts(copia2);
-    return 0;
+	}
+	printf("String copiada com strcpy:\n");
+	puts(copia1);
+	printf("String copiada com for:\n");
+	puts(copia2);
+	return 0;
 }
